5fdf626108868056c52d148670a226188d61c46e
Paulius Mačernis <sugalvojau@gmail.com>
2014-12-10 22:39:07 +0200

    Debug module initialization.

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,8 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-    ),
+        'Debug'
+        ),
 
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
@@ -14,7 +15,7 @@ return array(
         'module_paths' => array(
             './module',
             './vendor',
-        ),
+            ),
 
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively override configuration
@@ -61,4 +62,4 @@ return array(
    // Initial configuration with which to seed the ServiceManager.
    // Should be compatible with Zend\ServiceManager\Config.
    // 'service_manager' => array(),
-);
+    );
---
+++ module/Debug/config/module.config.php
@@ -0,0 +1,92 @@
+<?php
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Debug\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // Simply drop new controllers in, and you can access them
+            // using the path /debug/:controller/:action
+            'debug' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/debug',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Debug\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+        ),
+    ),
+//    'translator' => array(
+//        'locale' => 'en_US',
+//        'translation_file_patterns' => array(
+//            array(
+//                'type'     => 'gettext',
+//                'base_dir' => __DIR__ . '/../language',
+//                'pattern'  => '%s.mo',
+//            ),
+//        ),
+//    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Debug\Controller\Index' => 'Debug\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+//        'display_not_found_reason' => true,
+//        'display_exceptions'       => true,
+//        'doctype'                  => 'HTML5',
+//        'not_found_template'       => 'error/404',
+//        'exception_template'       => 'error/index',
+//        'template_map' => array(
+//            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+//            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+//            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+//            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+//        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+//    'console' => array(
+//        'router' => array(
+//            'routes' => array(
+//            ),
+//        ),
+//    ),
+//    'application' => array(
+//        'version'   => '[0.0.1]',
+//        'name'      => '[Application name]'
+//    )
+);
