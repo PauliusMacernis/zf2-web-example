38fa43113bb8a4fa73507e8932bb9d202ef8dabe
Paulius Mačernis <sugalvojau@gmail.com>
2014-12-10 23:55:30 +0200

    Added info on loaded modules. Such debuging info is going to the log.

--- module/Debug/Module.php
+++ module/Debug/Module.php
@@ -1,15 +1,32 @@
 <?php
+
 namespace Debug;
 
-class Module
-{
-    public function getConfig()
-    {
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\ModuleManager\ModuleManager;
+use Zend\EventManager\Event;
+
+class Module implements AutoloaderProviderInterface {
+
+    public function init(ModuleManager $moduleManager) {
+
+        $eventManager = $moduleManager->getEventManager();
+        $eventManager->attach('loadModules.post', array($this, 'loadedModulesInfo'));
+    }
+
+    public function loadedModulesInfo(Event $event) {
+
+        $moduleManager = $event->getTarget();
+        $loadedModules = $moduleManager->getLoadedModules();
+        error_log(var_export($loadedModules, true));
+    }
+
+    public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getAutoloaderConfig()
-    {
+    public function getAutoloaderConfig() {
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
@@ -18,4 +35,5 @@ class Module
             ),
         );
     }
+
 }
