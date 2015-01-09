83489fb67703181f4db94e04ffc153fc6d4b397e
Paulius Mačernis <sugalvojau@gmail.com>
2015-01-09 15:20:09 +0200

    Some updates on viewModel of Aplication/Controler/Index - now the Aplication/Controler/Index uses its own layout. Adding children to the layout has been prevented.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -19,11 +19,29 @@ class IndexController extends AbstractActionController
         $serviceLocator = $this->getServiceLocator();
         $config = $serviceLocator->get('config');
         
-        // return new ViewModel();
-        return array(
+        // layout will be our parent model
+        $layoutViewModel = new ViewModel();
+        // it must render view template with name 'layout/layout'
+        $layoutViewModel->setTemplate('layout/layout');
+        
+        
+        $viewModel = new ViewModel();
+        $viewModel->setVariables(array(
             'version' => $config['application']['version'],
             'applicationName' => $config['application']['name']
-        );
+        ));
+        $viewModel->setTemplate('application/index/about');
+        
+        // 
+        $layoutViewModel->setTerminal(true); // prevent adding children 
+        
+        // We add the view model as a child to the layout 
+        // The rendered content from the child model will be used
+        // as value for the content variable
+        $layoutViewModel->addChild($viewModel, 'content');
+        
+        return $layoutViewModel;
+        
     }
     
     public function aboutAction()
