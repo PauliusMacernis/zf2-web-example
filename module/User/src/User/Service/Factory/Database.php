255d9fc5d736157b4fb1539f46e8435acada39eb
Paulius Mačernis <sugalvojau@gmail.com>
2015-01-18 15:00:35 +0200

    Minor fix.

--- module/User/src/User/Service/Factory/Database.php
+++ module/User/src/User/Service/Factory/Database.php
@@ -1,19 +1,20 @@
 <?php
 
     namespace User\Service\Factory;
-    
+
     use Zend\ServiceManager\FactoryInterface;
     use Zend\Db\Adapter\Adapter as DbAdapter;
     use Zend\ServiceManager\ServiceLocatorInterface;
-    
-    class Database implements \Zend\ServiceManager\FactoryInterface {
-        
-        public function createService(ServiceLocatorInterface $serviceLocator) {
+
+    class Database implements FactoryInterface
+    {
+        public function createService(ServiceLocatorInterface $serviceLocator)
+        {
             $config = $serviceLocator->get('config');
             $adapter = new DbAdapter($config['db']);
-            
+
             return $adapter;
-            
+
         }
-        
+
     }
