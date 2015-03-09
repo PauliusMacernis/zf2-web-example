143e425f09e48dc491aef1b45fb91d79c59f148a
Paulius Mačernis <sugalvojau@gmail.com>
2015-03-10 00:45:05 +0200

    + The new module Exam has been added (generated with the help of zftool.phar).

--- config/application.config.php
+++ config/application.config.php
@@ -9,7 +9,8 @@ return array(
     'modules' => array(
         'Application',
         //'Debug',
-        'User'
+        'User',
+        'Exam'
         ),
     'module_listener_options' => array(
         'module_paths' => array(
