8a9daeff5d670822fcbccc7e72d0e0ff809ce364
Paulius Mačernis <sugalvojau@gmail.com>
2015-03-12 20:31:45 +0200

    + Info on configuration cache has been added.
    + Info on module map class cache has been added.
    + Info on class and template maps has been added.
    + filesystem cache enabled (predefined configuration file created to enable cache on local environment)

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -58,6 +58,7 @@ return array(
             'Zend\Log\LoggerAbstractServiceFactory',
         ),
         'factories' => array(
+            'text-cache' => 'Zend\Cache\Service\StorageCacheFactory',
             'cipher' => 'Application\Service\Factory\SymmetricCipher',
             'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
         ),
@@ -86,11 +87,12 @@ return array(
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
+        //'template_map' => include __DIR__ . '/../template_map.php'
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+                'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+                'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+                'error/404'               => __DIR__ . '/../view/error/404.phtml',
+                'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
